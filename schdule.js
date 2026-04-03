59c33d1652a379b4689c5b9847679ecd91b35362
SkipDss <ershov.gleb2020@gmail.com>
2026-04-03 10:16:42 +0400

    Add files via upload

--- schdule.js
+++ schdule.js
@@ -1,4 +1,7 @@
 let weekButtons = document.querySelectorAll(".button-week");
+let scheduleContainer = document.querySelector(".section-schedule__lessons");
+let scheduleTemplate = document.getElementById("lesson-card_template");
+let lessonElement = scheduleTemplate.content.cloneNode(true);
 
 weekButtons.forEach(weekButton => {
     weekButton.addEventListener("click", () => {
@@ -7,4 +10,20 @@ weekButtons.forEach(weekButton => {
     });
         weekButton.classList.toggle("button--active");
     })
+});
+
+
+const lessonsArray = [
+    { number: 1, subject: "ОСИС", timeStart: "09:00", timeEnd: "10:30", teacher: "Ферапонтов В.А.", room: "108" },
+    { number: 2, subject: "Физика", timeStart: "10:40", timeEnd: "12:10", teacher: "Гвоздикова О.С.", room: "408" },
+];
+
+lessonsArray.forEach(lesson => {
+    let lessonElement = scheduleTemplate.content.cloneNode(true);
+    lessonElement.querySelector('.lesson-number p').textContent = lesson.number;
+    lessonElement.querySelector('.lesson-card__subject').textContent = lesson.subject;
+    lessonElement.querySelector('.lesson-card__time').textContent = `${lesson.timeStart} - ${lesson.timeEnd}`;
+    lessonElement.querySelector('.lesson-card__teacher').textContent = lesson.teacher;
+    lessonElement.querySelector('.lesson-card__room-value').textContent = lesson.room;
+    scheduleContainer.appendChild(lessonElement);
 });
